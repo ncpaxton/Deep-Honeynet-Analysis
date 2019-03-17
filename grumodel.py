2f33d9f7a483890e6faf6c86ad3ee6ee787bfd6c
Berkay Polat <polatberkay25@gmail.com>
2019-03-16 19:03:55 -0700

    function name fix

--- grumodel.py
+++ grumodel.py
@@ -12,7 +12,7 @@ from keras.layers import  GRU, Dropout, Dense, Activation, TimeDistributed
 Building a RNN architecture with GRU
 recurrent_activation's defualt value is 'hard_sigmoid'
 """
-def build_bilstm_model(use_dropout, GRU_size, Dense_size, num_steps, num_features, num_classes, recurrent_activation='hard_sigmoid'):
+def build_gru_model(use_dropout, GRU_size, Dense_size, num_steps, num_features, num_classes, recurrent_activation='hard_sigmoid'):
     model = Sequential()
     model.add(GRU(GRU_size, activation='relu', return_sequences=True, input_shape=(num_steps,num_features), recurrent_activation=recurrent_activation))
     model.add(GRU(GRU_size, activation='relu', return_sequences=True, input_shape=(num_steps,num_features), recurrent_activation=recurrent_activation));
