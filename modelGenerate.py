860aff1f12375d089719d5919c7d8dcc8aff2c28
Berkay Polat <polatberkay25@gmail.com>
2019-03-17 23:10:03 -0700

    made predictions its own py script

--- modelGenerate.py
+++ modelGenerate.py
@@ -146,30 +146,6 @@ def get_loss_tables(history_lst, model_lst):
         val_loss_table[model_lst[i]] = history_lst[i].history['val_loss']
     return loss_table, val_loss_table
 
-"""
-Predictions using test set
-"""
-def predict(data_path, hyperparameters):
-    train_data, num_features = load_train_data('all_training_processed.csv')
-    test_data = load_test_data('all_test_processed.csv')
-    full_path = data_path + "/model-" + str(hyperparameters['num_epochs']) + ".hdf5"
-    model = load_model(full_path)
-    generator_parameters = {'batch_size': 1, 'num_classes':3, 'num_steps':25}
-    test_data_generator = KerasBatchGenerator(test_data, generator_parameters, num_features)
-    idx_to_label = {1: "Normal", 2: "Web", 0: "IoT"}
-    num_steps = hyperparameters['num_steps']
-    for i in range(0,test_data.shape[0], num_steps):
-        true_label = "True Label: "
-        predict_label = "Predicted: "
-        data = next(test_data_generator.generate())
-        predictions = model.predict(data[0])
-        predict_traffics = np.argmax(predictions, axis=-1)
-        for j in range(num_steps):
-            true_label += test_data[i+j,-1]
-            predict_label += idx_to_label[predict_traffics[j]]
-            print(true_label)
-            print(predict_label)
-
 
 model_lst = ['LSTM', 'GRU', 'BILSTM']
 history_lst = call_models(model_lst)
