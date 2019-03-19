e37e9b05848c47cf074c9acbc8ba74152ff23841
Berkay Polat <polatberkay25@gmail.com>
2019-03-18 21:40:01 -0700

    edit prediction.py script to calculate the accuracy

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
-import modelGenerate
+from modelGenerate import load_train_data, load_test_data, KerasBatchGenerator, get_hyperparameters
+from keras.models import load_model
+import numpy as np
 
 """
 Predictions using test set
@@ -10,16 +12,29 @@ def predict(data_path, hyperparameters):
     model = load_model(full_path)
     generator_parameters = {'batch_size': 1, 'num_classes':3, 'num_steps':25}
     test_data_generator = KerasBatchGenerator(test_data, generator_parameters, num_features)
-    idx_to_label = {1: "Normal", 2: "Web", 0: "IoT"}
+    #idx_to_label = {1: "Normal", 2: "Web", 0: "IoT"}
     num_steps = hyperparameters['num_steps']
+    correct_predictions = 0
+    
     for i in range(0,test_data.shape[0], num_steps):
-        true_label = "True Label: "
-        predict_label = "Predicted: "
         data = next(test_data_generator.generate())
         predictions = model.predict(data[0])
         predict_traffics = np.argmax(predictions, axis=-1)
         for j in range(num_steps):
-            true_label += test_data[i+j,-1]
-            predict_label += idx_to_label[predict_traffics[j]]
-            print(true_label)
-            print(predict_label)
+            if(i+j == test_data.shape[0]):
+                break
+            elif(test_data[i+j,-1] == predict_traffics[0][j]):
+                correct_predictions += 1
+            #true_label += idx_to_label[test_data[i+j,-1]]
+            #predict_label += idx_to_label[predict_traffics[0][j]]
+            #print(true_label)
+            #print(predict_label)
+            
+    return correct_predictions / test_data.shape[0]
+
+hyperparameters = get_hyperparameters()
+path_lst = ['lstm_model_history','gru_model_history','bilstm_model_history']
+
+for path in path_lst:
+    accuracy = predict(path, hyperparameters)
+    print(path + " accuracy: " + str(accuracy))
